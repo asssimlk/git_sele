19e637cd6531622dc05c849131723b774b5f8637
asssimlk <asimakakak11@outlook.com>
2025-01-18 00:58:52 +0530

    Update git_sele2.py

--- git_sele2.py
+++ git_sele2.py
@@ -68,7 +68,7 @@ def test_numbers(start, end):
 
 
 # Run the test
-test_numbers(7201, 7400)  # Adjust the range as needed
+test_numbers(8201, 8400)  # Adjust the range as needed
 
 # Close the driver after testing
 driver.quit()
